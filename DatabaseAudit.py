98f4c3d0c622ab6a460d78f217d0a90d115d8a8f
Samuel Drummond <sam.drummond@gmail.com>
2015-08-31 19:10:50 +1200

    Tidy ups and revisions to SDEReview

--- DatabaseAudit.py
+++ DatabaseAudit.py
@@ -1,132 +1,152 @@
 import os
 import arcpy
-import query_yes_no
 import csv
 from xml.etree.ElementTree import ElementTree  
+from query_yes_no import query_yes_no
 
 class SDEReview(object):
     
     def __init__(self, connectionString, output):
         arcpy.env.workspace = connectionString
         arcpy.env.scratchWorkspace = os.path.dirname(os.path.realpath(__file__))
-        this._output = output
+        self._output = output
         
-        desc = arcpy.describe(arcpy.env.workspace)
+        desc = arcpy.Describe(arcpy.env.workspace)
         connectionProperties = desc.connectionProperties
-        this._databaseName = connectionProperties.database
+        self._databaseName = connectionProperties.database
         
         dir = arcpy.GetInstallInfo("desktop")["InstallDir"]
-        this._metadataTranslator =  = dir + "Metadata/Translator/ESRI_ISO2ISO19139.xml"
+        self._metadataTranslator = dir + "Metadata/Translator/ESRI_ISO2ISO19139.xml"
+        
+        self._results = []
         
         print '### Welcome to SDE Review ###'
-        print 'You are about to review ' + this._databaseName
-        print 'Your report will be generated at ' + this._output
+        print 'You are about to review ' + self._databaseName
+        print 'Your report will be generated at ' + self._output
         
         raw_input('Hit any key to continue.')
         
-        this._reviewFeatureDatasets()
-        this._reviewFeatureClassesWithoutFeatureDatasets()
+        ##self._reviewFeatureDatasets()
+        self._reviewFeatureClassesWithoutFeatureDatasets()
         
-        this._generateOutputCSV()
+        self._generateOutputCSV()
         
     def _listFeatureDatasets(self):
         
-        this._datasets = arcpy.ListDatasets('*', 'FEATURES')
+        self._datasets = arcpy.ListDatasets('*', 'FEATURES')
         
     def _featureDatasetDesc(self, featureDatasetDesc):
         
         featureDatasetDescList = featureDatasetDesc.split('.')
+        self._currentFeatureDataset = featureDatasetDescList[2]
         
-        this._currentSchema = featureDatasetDescList[1]
-        this._currentFeatureDataset = featureDatasetDescList[2]
-        
-        print('You are about to review the ' + this._currentFeatureDataset + ' which is in the ' + this._currentSchema + 'schema.')
+        print('You are about to review the ' + self._currentFeatureDataset)
         
     def _areFieldsCamelCased(self, featureClass):
         
         print("Review Fields:")
         fields = arcpy.ListFields(featureClass)
-        for field in fields
+        for field in fields:
             print field.name
         
-        this._isCamelCased = query_yes_no("Are you satisfied that these fields are camel cased? Y/N")
+        self._isCamelCased = query_yes_no("Are you satisfied that these fields are camel cased? Y/N")
         
     def _reviewMetadata(self, featureClass):
         
-        xmlOutputPath =a rcpy.env.scratchWorkspace + featureClass.name + ".xml"
-        xmlFile = arcpy.ExportMetadata_conversion (featureClasses[0], this._metadataTranslator, xmlOutputPath)
+        xmlOutputPath = arcpy.env.scratchWorkspace + "\\featureClass.xml"
+
+        if os.path.isfile(xmlOutputPath): 
+            os.remove(xmlOutputPath)
+            
+        arcpy.ExportMetadata_conversion (featureClass, self._metadataTranslator, xmlOutputPath)
+        xmlFile = open(xmlOutputPath,'r')
         tree = ElementTree()
-        tree.parse(xmlfile)
+        tree.parse(xmlFile)
         root = tree.getroot()
         
-        print("Review Metadata")
+        print("Reviewing Metadata")
         
         titleElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}citation/{http://www.isotc211.org/2005/gmd}CI_Citation/{http://www.isotc211.org/2005/gmd}title/{http://www.isotc211.org/2005/gco}CharacterString"
         title = root.find(titleElement)
-        print("Title: " + title.text)
         
-        summaryElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}resourceMaintenance/{http://www.isotc211.org/2005/gmd}MD_MaintenanceInformation/{http://www.isotc211.org/2005/gmd}maintenanceAndUpdateFrequency/{http://www.isotc211.org/2005/gmd}MD_MaintenanceFrequencyCode"
+        summaryElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}purpose/{http://www.isotc211.org/2005/gco}CharacterString"
         summary = root.find(summaryElement)
-        print("Summary: " + summary.text)
         
-        constraintsElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}supplementalInformation/{http://www.isotc211.org/2005/gco}CharacterString
+        constraintsElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}supplementalInformation/{http://www.isotc211.org/2005/gco}CharacterString"
         constraints = root.find(constraintsElement)
-        print("Constraints: " + constraints.text)
         
         lateUpdatedElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}citation/{http://www.isotc211.org/2005/gmd}CI_Citation/{http://www.isotc211.org/2005/gmd}date/{http://www.isotc211.org/2005/gmd}CI_Date/{http://www.isotc211.org/2005/gmd}date/{http://www.isotc211.org/2005/gco}DateTime"
         lastUpdated = root.find(lateUpdatedElement)
-        print("Last Updated: " + lastUpdated.text)
         
         updateFrequencyElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}resourceMaintenance/{http://www.isotc211.org/2005/gmd}MD_MaintenanceInformation/{http://www.isotc211.org/2005/gmd}maintenanceAndUpdateFrequency/{http://www.isotc211.org/2005/gmd}MD_MaintenanceFrequencyCode"
         updateFrequency = root.find(updateFrequencyElement)
-        print("Update Frequency: " + updateFrequency.attrib['codeListValue'])
         
         pointOfContactElement = "{http://www.isotc211.org/2005/gmd}identificationInfo/{http://www.isotc211.org/2005/gmd}MD_DataIdentification/{http://www.isotc211.org/2005/gmd}pointOfContact/{http://www.isotc211.org/2005/gmd}CI_ResponsibleParty/{http://www.isotc211.org/2005/gmd}individualName/{http://www.isotc211.org/2005/gco}CharacterString"
         pointOfContact = root.find(pointOfContactElement)
-        print("Point of Contact: " + pointOfContact)
         
-        this._hasMetadata = query_yes_no("Are you satisfied that the feature class has metadata? Y/N")
+        hasMetadata = (hasattr(title, "text") AND hasattr(summary, "text") AND hasattr(constraints, "text") AND hasattr(lastUpdated, "text") AND hasattr(updateFrequency, "attrib") AND hasattr(pointOfContact, "text"))
         
+        if has:
+        
+            self._hasMetadata = "Yes"
+            
+        elif hasattr(title, "text") OR hasattr(summary, "text") OR hasattr(constraints, "text") OR hasattr(lastUpdated, "text") OR hasattr(updateFrequency, "attrib") OR hasattr(pointOfContact, "text"):
+            
+            self._hasMetadata = "Partial"
+                
+        else:
+            
+            self._hasMetadata = "NO"
+       
+        print("Has Metadata: " self._hasMetadata)
+        
+        xmlFile.close()
         os.remove(xmlOutputPath)
+            
         
     def _reviewFeatureDatasets(self):
         
-        this._listFeatureDatasets()
+        self._listFeatureDatasets()
         
-        for(featureDataset in this._datasets):
+        for featureDataset in self._datasets:
             
             featureClasses = arcpy.ListFeatureClasses(feature_dataset=featureDataset)
-            this._featureDatasetDesc(featureDataset)
-            this._reviewFeatureClasses(featureClasses)
+            self._featureDatasetDesc(featureDataset)
+            self._reviewFeatureClasses(featureClasses)
     
     def _reviewFeatureClassesWithoutFeatureDatasets(self):
         
-        this._featureDataset = "none"
+        self._currentFeatureDataset = "none"
         featureClasses = arcpy.ListFeatureClasses()
-        this._featureDatasetDesc(featureDataset)
-        this._reviewFeatureClasses(featureClasses)
+        
+        self._reviewFeatureClasses(featureClasses)
 
-    def _reviewFeatureClasses(featureClasses):
+    def _reviewFeatureClasses(self, featureClasses):
         for featureClass in featureClasses:
-            print("Feature Class name" = featureClass.name)
-            this._areFieldsCamelCased(featureClass);
-            this._reviewMetadata(featureClass);
+            print("Feature Class name: " + featureClass)
+
+            self._areFieldsCamelCased(featureClass);
+            self._reviewMetadata(featureClass);
+            
+            featureClassNameList = featureClass.split(".")
+            self._currentSchema = featureClassNameList[1]
+            tidyFeatureClassName = featureClassNameList[2]
             
             featureClassRecord = [
-                this._databaseName,
-                this._currentSchema,
-                this._featureDataset, #need to think about this one - for feature classes that don't have one
-                featureClass.name,
-                this._isCamelCased,
-                this._hasMetadata
+                self._databaseName,
+                self._currentSchema,
+                self._currentFeatureDataset,
+                tidyFeatureClassName,
+                self._isCamelCased,
+                self._hasMetadata
             ]
             
-            this._results.append(featureClassRecord)
+            self._results.append(featureClassRecord)
 
     def _generateOutputCSV():
         
-        outputFileCSV = open(this._output, 'wb')
+        outputFileCSV = open(self._output, 'wb')
         wr = csv.writer(outputFileCSV, quoting=csv.QUOTE_ALL)
-        wr.writerows(this._results)
+        wr.writerows(self._results)
         
         
